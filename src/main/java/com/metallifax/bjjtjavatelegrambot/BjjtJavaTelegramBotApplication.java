7ad8ca583727fa076cfdffc6c27f7b307af9400a
Metallifax <mathew_forgione@msn.com>
2022-04-13 18:22:23 -0300

    initial version

---
+++ src/main/java/com/metallifax/bjjtjavatelegrambot/BjjtJavaTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.metallifax.bjjtjavatelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjjtJavaTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BjjtJavaTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/metallifax/bjjtjavatelegrambot/Bot.java
@@ -0,0 +1,44 @@
+package com.metallifax.bjjtjavatelegrambot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+    String BOT_TOKEN;
+    String BOT_USERNAME;
+
+    Bot(@Value("${bot.BOT_TOKEN}") String BOT_TOKEN, @Value("${bot.BOT_USERNAME}") String BOT_USERNAME) {
+        this.BOT_TOKEN = BOT_TOKEN;
+        this.BOT_USERNAME = BOT_USERNAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_USERNAME;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage();
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setReplyToMessageId(update.getMessage().getMessageId());
+            message.setText(update.getMessage().getText());
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
